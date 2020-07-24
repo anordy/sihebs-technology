bd0ca29e3fac4ed5ba3713b148bbdf06a2f4da1a
anordy <anordypatrick98@gmail.com>
2020-07-24 03:12:53 +0300

    homepage

---
+++ custom.js
@@ -0,0 +1,19 @@
+// slide show
+$(".carousel").carousel({
+    interval: 2000,
+  });
+
+
+// fixed header
+    // window.onscroll = function() {myFunction()};
+
+    // var header = document.getElementById("myHeader");
+    // var sticky = header.offsetTop;
+
+    // function myFunction() {
+    //   if (window.pageYOffset > sticky) {
+    //     header.classList.add("sticky");
+    //   } else {
+    //     header.classList.remove("sticky");
+    //   }
+    // }

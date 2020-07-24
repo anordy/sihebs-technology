3056ec95e0ead84b113d25a992ba8752745c689d
anordy <anordypatrick98@gmail.com>
2020-07-24 14:43:28 +0300

    pics

--- custom.js
+++ custom.js
@@ -4,16 +4,3 @@ $(".carousel").carousel({
   });
 
 
-// fixed header
-    // window.onscroll = function() {myFunction()};
-
-    // var header = document.getElementById("myHeader");
-    // var sticky = header.offsetTop;
-
-    // function myFunction() {
-    //   if (window.pageYOffset > sticky) {
-    //     header.classList.add("sticky");
-    //   } else {
-    //     header.classList.remove("sticky");
-    //   }
-    // }
